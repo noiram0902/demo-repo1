93219a91418055ae00f6b0ab76bd318cc04b5b49
GitPracticeAccount-Marion <88105743+noiram0902@users.noreply.github.com>
2025-04-17 10:07:26 +0800

    Update HelloWorldTrgCopy.java

--- HelloWorldTrgCopy.java
+++ HelloWorldTrgCopy.java
@@ -1,4 +1,4 @@
 Trigger HelloWorldTrg on Account(before insert,before update){
 	//System.debug('Hello World 123' + 'Updated');
-	//System.debug('This is added');
+	//System.debug('This is added' + 'NEW UPDATE');
 }
