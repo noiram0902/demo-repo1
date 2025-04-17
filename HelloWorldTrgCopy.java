86d16f0d12e3038275fa36a632a7d047a29b3554
Yones <mlegacion0902@gmail.com>
2025-04-17 10:25:28 +0800

    Update HelloWorldTrgCopy.java

--- HelloWorldTrgCopy.java
+++ HelloWorldTrgCopy.java
@@ -1,4 +1,4 @@
 Trigger HelloWorldTrg on Account(before insert,before update){
 	//System.debug('Hello World 123' + 'Updated');
-	//System.debug('This is added' + 'NEW UPDATE');
+	//System.debug('This is added' + 'NEW UPDATE AGAIN');
 }
